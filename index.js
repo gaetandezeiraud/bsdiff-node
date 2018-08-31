9abf453c5e0d48ec83b5ba735e726f082929dbb0
gucong <gucong@gmail.com>
2018-08-31 18:51:39 +0800

    Add support for async function

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+'use strict';
 const bsdiff = require('./build/Release/bsdiff.node');
+function promisify (fnName) {
+  const fn = bsdiff[fnName];
+  bsdiff[fnName] = function () {
+    let args = Array.from(arguments);
+    let callback;
+    if (typeof args[args.length - 1] === 'function') {
+      callback = args.pop();
+    }
+    return new Promise(function (resolve, reject) {
+      args.push(function (result, err) {
+        if (err) {
+          reject(err);
+        } else if (result >= 100) {
+          process.nextTick(resolve);
+        }
+        if (callback) {
+          callback(result, err);
+        }
+      });
+      fn.apply(bsdiff, args);
+    });
+  };
+}
+
+promisify('diff');
+promisify('patch');
+
 module.exports = bsdiff;
--- test/test.js
+++ test/test.js
@@ -1,35 +1,31 @@
+'use strict';
 const path = require('path');
+const fs = require('fs');
 const bsdiff = require('../index');
 
 describe('bsdiff', () => {
+  const oldFile = path.join(__dirname, 'resources/react-0.3-stable.zip');
+  const newFile = path.join(__dirname, 'resources/react-0.4-stable.zip');
+  const patchFile = path.join(__dirname, 'resources/react.patch');
+  const generatedFile = path.join(__dirname, 'resources/react-generated.zip');
+
+  afterEach(() => {
+    fs.unlinkSync(patchFile);
+    fs.unlinkSync(generatedFile);
+  });
+
   describe('#diff() + #patch() async', () => {
-    it('should generate an file identical to original file after appling a patch to an old file', () => {
-      const oldFile = path.join(__dirname, 'resources/react-0.3-stable.zip');
-      const newFile = path.join(__dirname, 'resources/react-0.4-stable.zip');
-      const patchFile = path.join(__dirname, 'resources/react.patch');
-      const generatedFile = path.join(__dirname, 'resources/react-generated.zip');
-
-      bsdiff.diff(oldFile, newFile, patchFile, function(result, err) {
-          if(err) return console.log('Error:', err);
-          console.log(result + "%");
-
-          if(result === 100) {
-            console.log("Patch");
-            bsdiff.patch(oldFile, generatedFile, patchFile, function(result, err) {
-                if(err) return console.log('Error:', err);
-                console.log(result + "%");
-            });
-          }
+    it('should generate an file identical to original file after appling a patch to an old file', async () => {
+      await bsdiff.diff(oldFile, newFile, patchFile, function (result) {
+        console.log('diff:' + String(result).padStart(4) + '%');
+      });
+      await bsdiff.patch(oldFile, generatedFile, patchFile, function (result) {
+        console.log('patch:' + String(result).padStart(4) + '%');
       });
     });
   });
   describe('#diff() + #patch() sync', () => {
     it('should generate an file identical to original file after appling a patch to an old file', () => {
-      const oldFile = path.join(__dirname, 'resources/react-0.3-stable.zip');
-      const newFile = path.join(__dirname, 'resources/react-0.4-stable.zip');
-      const patchFile = path.join(__dirname, 'resources/react.patch');
-      const generatedFile = path.join(__dirname, 'resources/react-generated.zip');
-
       bsdiff.diffSync(oldFile, newFile, patchFile);
       bsdiff.patchSync(oldFile, generatedFile, patchFile);
     });
